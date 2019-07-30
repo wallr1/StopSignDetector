8de2ae23b56e5cd3349bd03b7ec6ab922d8f896a
Wall <wallr1@wit.edu>
2019-07-30 15:00:53 -0400

    return value for success in detection

--- src/stopSignDetector.m
+++ src/stopSignDetector.m
@@ -1,9 +1,13 @@
-function procImg = stopSignDetector(I)
+function [status , procImg] = stopSignDetector(I)
+    status = 1;
     imL = length(I(:,1));
     cd('../models');
-    minS = [round(imL/5) round(imL/5)];
-    detector = vision.CascadeObjectDetector('stopSignDetector.xml', 'MinSize', minS);
+    minS = [round(imL/7) round(imL/7)];
+    detector = vision.CascadeObjectDetector('stopSignDetector.xml');%, 'MinSize', minS);
     bbox = step(detector,I);
+    if isempty(bbox)
+        status = 0;
+    end
     cd('../src');
     lineW = round(imL / 100);
     fontS = lineW*4;
@@ -11,5 +15,3 @@ function procImg = stopSignDetector(I)
         fontS = 72;
     end
     procImg =  insertObjectAnnotation(I,'rectangle',bbox,'stop sign','LineWidth',lineW,'FontSize',fontS);
-    
-%     imshow(procImg);
