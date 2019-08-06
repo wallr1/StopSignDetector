7077bb927e224628ccd81bfeec845dbcc8c43027
Charlie Pluk <charliepluk@users.noreply.github.com>
2019-08-06 15:32:31 -0400

    Added test images

--- src/GUI.m
+++ src/GUI.m
@@ -78,7 +78,7 @@ function pushbutton1_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-cd('../positiveImages');
+cd('../images');
 image = uigetfile(fullfile('*'));
 axes(handles.axes1);
 image = imread(image);
